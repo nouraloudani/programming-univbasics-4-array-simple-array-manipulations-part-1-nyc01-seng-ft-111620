64f748ecfdb7f382e57b41f85d7838fed4de0bab
Noura Loudani <contact.nouraloudani@gmail.com>
2020-11-04 21:43:58 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -5,3 +5,19 @@ end
 def using_unshift(array, string)
   
 end
+
+def using_pop(array, string)
+  
+end
+
+def pop_with_args(array, string)
+  
+end
+
+def using_shift(array)
+  
+end
+
+def using_unshift(array, string)
+  
+end
